1737f242ff2a8ddf22bed0840c4ffbb31c13e921
Mattias Schlenker <ms@mattiasschlenker.de>
2014-06-16 13:58:47 +0200

    add first binaries in stage02

---
+++ stage02/0005_zlib.sh
@@ -0,0 +1,29 @@
+
+source stage01_variables
+source stage02_variables
+ 
+PKGNAME=zlib
+PKGVERSION=1.2.8
+
+# Download:
+
+[ -f ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz ] || wget -O ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz \
+	http://downloads.sourceforge.net/project/libpng/zlib/${PKGVERSION}/${PKGNAME}-${PKGVERSION}.tar.xz
+
+# Prepare build:
+
+mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+tar xvJf ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz
+
+# Build and install
+
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+CFLAGS="-Os" ./configure --shared
+make 
+make prefix=${CLFS}/cross-tools/${CLFS_TARGET} install
+
+# Clean up
+
+rm -rf ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+
---
+++ stage02/0010_busybox.sh
@@ -0,0 +1,32 @@
+
+source stage01_variables
+source stage02_variables
+ 
+PKGNAME=busybox
+PKGVERSION=1.22.1
+
+# Download:
+
+[ -f ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.bz2 ] || wget -O ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.bz2 \
+	http://busybox.net/downloads/${PKGNAME}-${PKGVERSION}.tar.bz2
+
+# Prepare build:
+
+mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+tar xvjf ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.bz2
+
+# Build and install
+
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+ARCH="${CLFS_ARCH}" make defconfig
+sed -i 's/\(CONFIG_\)\(.*\)\(INETD\)\(.*\)=y/# \1\2\3\4 is not set/g' .config
+sed -i 's/\(CONFIG_IFPLUGD\)=y/# \1 is not set/' .config
+ARCH="${CLFS_ARCH}" CROSS_COMPILE="${CLFS_TARGET}-" make
+ARCH="${CLFS_ARCH}" CROSS_COMPILE="${CLFS_TARGET}-" make  \
+  CONFIG_PREFIX="${CLFS}/targetfs" install
+
+# Clean up
+
+rm -rf ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+
