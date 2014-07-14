5ec5205b9c0062ced10867fb2abefe0d30ebaf6e
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-14 15:36:10 +0200

    add minimal boot scripts

--- stage02/0001_basefiles.sh
+++ stage02/0001_basefiles.sh
@@ -10,4 +10,4 @@ PKGVERSION=20140714
 mkdir -p ${CLFS}/targetfs/etc/rc.d
 install -m 0644 patches/etc-inittab ${CLFS}/targetfs/etc/inittab
 install -m 0755 patches/etc-rc ${CLFS}/targetfs/etc/rc
-install -m 0755 patches/etc-rc-shutdown ${CLFS}/targetfs/etc/rc.shutdown
+install -m 0755 patches/etc-rc.shutdown ${CLFS}/targetfs/etc/rc.shutdown
