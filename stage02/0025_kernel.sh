ff660e2a006ce55928cd8c587f3ebfd662f5ef50
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-17 15:40:40 +0200

    enable 32 bit build on x86

--- stage02/0025_kernel.sh
+++ stage02/0025_kernel.sh
@@ -14,27 +14,40 @@ PKGVERSION=3.15.5
         https://www.kernel.org/pub/linux/kernel/v3.x/patch-${PKGVERSION}.xz
 
 # Prepare build:
-
 wdir=` pwd `
 mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
 cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}
 tar xvJf ${SRCDIR}/${PKGNAME}-3.15.tar.xz
 cd ${PKGNAME}-3.15
 unxz -c ${SRCDIR}/patch-${PKGVERSION}.xz | patch -p1
-cp -v "${wdir}/patches/config-3.15.1" .config
+
+# Use the variable TINYKCONFIG to specify a custom kernel configuration!
+KCONFIG="${wdir}/patches/config-3.15.1"
+[ -n "$TINYKCONFIG" ] && KCONFIG="$TINYKCONFIG"
+cp -v "$KCONFIG" .config
+
 cd ..
 mv ${PKGNAME}-3.15 ${PKGNAME}-${PKGVERSION}
 
 # Build and install
+TINYARCH=x86_64
+case ${CLFS_TARGET} in
+	x86_64*)
+		TINYARCH=x86_64
+	;;
+	i?86*)
+		TINYARCH=x86
+	;;
+esac
 
 cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
 localversion=` grep '^CONFIG_LOCALVERSION' .config | sed 's/"//g' | awk -F '=' '{print $2}' `
-make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-musl- oldconfig
-make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-musl-
-INSTALL_MOD_PATH=${CLFS}/targetfs make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-musl- modules_install || exit 1 
+make ARCH=${TINYARCH} CROSS_COMPILE=${CLFS_TARGET}- oldconfig
+make ARCH=${TINYARCH} CROSS_COMPILE=${CLFS_TARGET}-
+INSTALL_MOD_PATH=${CLFS}/targetfs make ARCH=${TINYARCH} CROSS_COMPILE=${CLFS_TARGET}- modules_install || exit 1 
 mkdir -p ${CLFS}/targetfs/boot
 install -m 0644 arch/x86/boot/bzImage ${CLFS}/targetfs/boot/vmlinuz-${PKGVERSION}${localversion} || exit 1
-find ${CLFS}/targetfs/lib/modules/${PKGVERSION}${localversion} -type f -name '*.ko' -exec x86_64-linux-musl-strip --strip-unneeded {} \; 
+find ${CLFS}/targetfs/lib/modules/${PKGVERSION}${localversion} -type f -name '*.ko' -exec ${CLFS_TARGET}-strip --strip-unneeded {} \; 
 
 # Clean up
 
