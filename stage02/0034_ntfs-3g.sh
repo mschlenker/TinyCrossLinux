a7fcd581dce4391d16cdabee38401fb6dedd136d
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-21 08:11:24 +0200

    fix pkgconfig

--- stage02/0034_ntfs-3g.sh
+++ stage02/0034_ntfs-3g.sh
@@ -24,6 +24,7 @@ sed -i 's/ linux-uclibc\*/linux-uclibc\* | linux-musl\*/g' ./config.sub
 
 export FUSE_MODULE_CFLAGS="-D_FILE_OFFSET_BITS=64 -I${CLFS}/cross-tools/${CLFS_TARGET}/include"
 export FUSE_MODULE_LIBS='-pthread -lfuse'
+export CFLAGS="-I${CLFS}/cross-tools/${CLFS_TARGET}/include"
 ./configure --prefix=/usr --host=${CLFS_TARGET} --with-fuse=external
 make || exit 1
 make prefix=${CLFS}/targetfs install || exit 1
