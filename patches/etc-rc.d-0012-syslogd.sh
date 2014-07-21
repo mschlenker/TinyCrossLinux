2d35d4c9dc89a1511311ede1bf6215a24387ac22
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-21 10:37:16 +0200

    may or may not start

--- patches/etc-rc.d-0012-syslogd.sh
+++ patches/etc-rc.d-0012-syslogd.sh
@@ -2,8 +2,18 @@
 
 case $1 in
     start)
-	printf "${bold}Starting syslogd ${normal}"
-	mkdir -p /var/log 
-	syslogd && printf "${success}\n" || printf "${failed}\n"
+	enable=1
+	for tok in ` cat /proc/cmdline ` ; do
+		case $tok in
+			syslogd=0)
+				enable=0
+			;;
+		esac
+	done
+	if [ "$enable" -gt 0 ] ; then
+		printf "${bold}Starting syslogd ${normal}"
+		mkdir -p /var/log 
+		syslogd && printf "${success}\n" || printf "${failed}\n"
+	fi
     ;;
 esac
