23fd1cbc9d69f3b7d00b3605d051a83775bd0f7d
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-21 08:26:02 +0200

    workaround missing header

--- stage02/0034_ntfs-3g.sh
+++ stage02/0034_ntfs-3g.sh
@@ -24,7 +24,6 @@ sed -i 's/ linux-uclibc\*/linux-uclibc\* | linux-musl\*/g' ./config.sub
 
 export FUSE_MODULE_CFLAGS="-D_FILE_OFFSET_BITS=64 -I${CLFS}/cross-tools/${CLFS_TARGET}/include"
 export FUSE_MODULE_LIBS='-pthread -lfuse'
-cp -v ${CLFS}/cross-tools/${CLFS_TARGET}/include/fuse_lowlevel.h src/
 ./configure --prefix=/usr --host=${CLFS_TARGET} --with-fuse=external
 make || exit 1
 make prefix=${CLFS}/targetfs install || exit 1
