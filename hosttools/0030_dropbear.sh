f4d83567b6cd3e19b3d7b171f0d6833b04ce48e9
Mattias Schlenker <ms@mattiasschlenker.de>
2014-07-31 12:16:01 +0200

    add dropbear to hosttools

---
+++ hosttools/0030_dropbear.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# A complete dropbear installation should be present for the host to be able
+# to create or convert keys that are included in the initramfs.
+
+source stage0n_variables
+
+PKGNAME=dropbear-host
+PKGVERSION=2014.63
+
+# Download:
+
+[ -f ${SRCDIR}/dropbear-${PKGVERSION}.tar.bz2 ] || wget -O ${SRCDIR}/dropbear-${PKGVERSION}.tar.bz2 \
+	https://matt.ucc.asn.au/dropbear/dropbear-${PKGVERSION}.tar.bz2
+
+# Prepare build:
+
+mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+tar -C ${CLFS}/build/${PKGNAME}-${PKGVERSION} -xvjf ${SRCDIR}/dropbear-${PKGVERSION}.tar.bz2
+
+# Build and install
+
+workdir=` pwd ` 
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/dropbear-${PKGVERSION}
+sed -i 's/.*mandir.*//g' Makefile.in
+./configure --prefix=${CLFS}/hosttools
+make MULTI=1 PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" || exit 1
+make MULTI=1 PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" install || exit 1
+
+# Clean up
+
+cd ..
+rm -rf ${CLFS}/build/${PKGNAME}-${PKGVERSION}/dropbear-${PKGVERSION}
