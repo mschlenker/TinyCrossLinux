8838ab251b559ea14f8dcc38fb15195cfd6eb43a
Mattias Schlenker <ms@mattiasschlenker.de>
2014-12-15 11:44:38 +0100

    add qemu

---
+++ hosttools/0035_qemu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# A complete dropbear installation should be present for the host to be able
+# to create or convert keys that are included in the initramfs.
+
+source stage0n_variables
+
+PKGNAME=qemu-host
+PKGVERSION=2.2.0
+
+# Download:
+
+[ -f ${SRCDIR}/qemu-${PKGVERSION}.tar.bz2 ] || wget -O ${SRCDIR}/qemu-${PKGVERSION}.tar.bz2 \
+	http://wiki.qemu-project.org/download/qemu-${PKGVERSION}.tar.bz2
+
+# Prepare build:
+
+mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+tar -C ${CLFS}/build/${PKGNAME}-${PKGVERSION} -xvjf ${SRCDIR}/qemu-${PKGVERSION}.tar.bz2
+
+# Build and install
+
+workdir=` pwd ` 
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/qemu-${PKGVERSION}
+./configure --prefix=${CLFS}/hosttools \
+	--docdir=/usr/share/doc/${PKGNAME}-${PKGVERSION} \
+	--disable-bsd-user --disable-linux-user \
+	--target-list=x86_64-softmmu,i386-softmmu \
+	--disable-sdl --disable-vnc --audio-drv-list=alsa --disable-gtk \
+	--disable-smartcard-nss
+
+make -j ` grep -c processor /proc/cpuinfo `  || exit 1
+make install || exit 1
+
+# Clean up
+
+cd ..
+rm -rf ${CLFS}/build/${PKGNAME}-${PKGVERSION}/qemu-${PKGVERSION}
