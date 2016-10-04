87e971f8fe9dbe8de947227c5945bc87819a8a3b
Mattias Schlenker <ms@mattiasschlenker.de>
2016-10-04 17:11:09 +0200

    fix build with gcc5

--- hosttools/0010_syslinux.sh
+++ hosttools/0010_syslinux.sh
@@ -18,17 +18,21 @@ esac
  
 [ -f ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz ] || wget -O ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz \
 	https://www.kernel.org/pub/linux/utils/boot/syslinux/${PKGNAME}-${PKGVERSION}.tar.xz
-
+[ -f ${SRCDIR}/${PKGNAME}_${PKGVERSION}+dfsg-11ubuntu1.debian.tar.xz || wget -O ${PKGNAME}_${PKGVERSION}+dfsg-11ubuntu1.debian.tar.xz \
+	https://launchpad.net/ubuntu/+archive/primary/+files/${PKGNAME}_${PKGVERSION}+dfsg-11ubuntu1.debian.tar.xz
 # Prepare build:
 
 mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
 tar -C ${CLFS}/build/${PKGNAME}-${PKGVERSION} -xvJf ${SRCDIR}/${PKGNAME}-${PKGVERSION}.tar.xz || exit 1
+tar -C ${CLFS}/build/${PKGNAME}-${PKGVERSION} -xvf ${SRCDIR}/${PKGNAME}_${PKGVERSION}+dfsg-11ubuntu1.debian.tar.xz
+
+# Patch 
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+cat ../debian/patches/series | while read fname ; do cat ../debian/patches/$fname | patch -p1; done
 
 # Build and install
-
-cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
-# make clean
-# make || exit 1
+make clean
+make || exit 1
 mkdir -p ${CLFS}/hosttools/share/syslinux
 tar -C ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION} -cvf - . | tar -C ${CLFS}/hosttools/share/syslinux -xf - 
 
