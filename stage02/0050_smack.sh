9886aa6db1c1fa2646f9e4f9820c70e3f1b81ed3
Mattias Schlenker <ms@mattiasschlenker.de>
2017-01-19 12:19:33 +0100

    add smack

---
+++ stage02/0050_smack.sh
@@ -0,0 +1,32 @@
+
+source stage0n_variables
+source stage01_variables
+source stage02_variables
+ 
+PKGNAME=smack
+PKGVERSION=1.3.0
+
+# Download:
+# https://github.com/smack-team/smack/archive/v1.3.0.tar.gz
+
+[ -f ${SRCDIR}/v${PKGVERSION}.tar.gz ] || wget -O ${SRCDIR}/v${PKGVERSION}.tar.gz \
+	https://github.com/smack-team/smack/archive/v${PKGVERSION}.tar.gz
+
+# Prepare build:
+
+mkdir -p ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}
+tar xvJf ${SRCDIR}/v${PKGVERSION}.tar.gz
+
+# Build and install
+
+cd ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+bash autogen.sh
+./configure --prefix=/usr --host=${CLFS_TARGET}
+make || exit 1
+make install DESTDIR=${CLFS}/targetfs || exit 1
+
+# Clean up
+
+rm -rf ${CLFS}/build/${PKGNAME}-${PKGVERSION}/${PKGNAME}-${PKGVERSION}
+
